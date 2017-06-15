05143a4d2958428df0bb7e236c867cd3f72d2b04
Soeren Vibjerg <vibjerg@dhcp279.dbc.dk>
2017-06-15 14:17:04 +0200

    add Booster selector component

--- src/components/booster.js
+++ src/components/booster.js
@@ -1,9 +1,65 @@
 import React from 'react';
+import GlobalState from '../GlobalState';
 
-export default function Booster({name, value, set}) {
+function PopularityBooster({value, change}) {
   return (
     <div>
-      <input defaultValue={value} type="text" onChange={e => set(e.currentTarget.value)}/>
+      <div className="booster--name">Popularity Booster</div>
+      <div className="booster--value">{value}</div>
+      <input defaultValue={value.factor} type="range" min='0' max="1000" onChange={e => change({factor: Number(e.currentTarget.value), name: 'popularityBooster'})} />
     </div>
   );
+}
+
+PopularityBooster.defaultValue = {factor: name, name: 'popularityBooster'};
+
+const boosterList = {PopularityBooster};
+
+export default class Boosters extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      boosters: [
+      ]
+    };
+  }
+
+  submit = (e) => {
+    const booster = boosterList[this.refs.select.value];
+    const boosters = this.state.boosters.concat({
+      Booster: booster,
+      value: booster.defaultValue
+    });
+    this.setState({boosters});
+    e.preventDefault();
+  }
+
+  changeBoosterValue(booster, value) {
+    const boosters = this.state.boosters.map(b => {
+      if (b.Booster == booster) {
+        b.value = value;
+      }
+      return b;
+    });
+    this.setState({boosters});
+    const values = boosters.map(b => b.value);
+    GlobalState.setState({boosters: values});
+  }
+
+  render() {
+    return (
+      <div>
+      <form className="boosters" onSubmit={this.submit}>
+        <select ref="select" name="boost-selector" id="" defaultValue={PopularityBooster}>
+          {Object.keys(boosterList).map(boosterName => <option value={boosterName}>{boosterName}</option>)}
+        </select>
+        <input type="submit" value="opret"/>
+      </form>
+        <div className="boosters">
+          {this.state.boosters.map(({Booster, value}, i) => <Booster key={Booster} value change={value => this.changeBoosterValue(Booster, value)} />)}
+        </div>
+      </div>
+    );
+  }
 }
